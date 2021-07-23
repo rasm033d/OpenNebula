1568df94f34d3452510c0724d3e33aa5ff1e9912
rasm033d <54931680+rasm033d@users.noreply.github.com>
2021-07-23 18:28:07 +0200

    Create Parted.sh

---
+++ Parted.sh
@@ -0,0 +1,16 @@
+/sbin/parted /dev/sda mklabel gpt --script
+/sbin/parted /dev/sda mkpart primary 0% 100% --script
+/sbin/parted /dev/sdb mklabel gpt --script
+/sbin/parted /dev/sdb mkpart primary 0% 100% --script
+/sbin/parted /dev/sdc mklabel gpt --script
+/sbin/parted /dev/sdc mkpart primary 0% 100% --script
+/sbin/parted /dev/sdd mklabel gpt --script
+/sbin/parted /dev/sdd mkpart primary 0% 100% --script
+/sbin/parted /dev/sde mklabel gpt --script
+/sbin/parted /dev/sde mkpart primary 0% 100% --script
+/sbin/parted /dev/sdf mklabel gpt --script
+/sbin/parted /dev/sdf mkpart primary 0% 100% --script
+/sbin/parted /dev/sdg mklabel gpt --script
+/sbin/parted /dev/sdg mkpart primary 0% 100% --script
+/sbin/parted /dev/sdh mklabel gpt --script
+/sbin/parted /dev/sdh mkpart primary 0% 100% --script
