45533aadf29dcbcba40788651f1472926592b0c6
rasm033d <54931680+rasm033d@users.noreply.github.com>
2021-07-23 18:28:31 +0200

    Create LVMCacheCreate.sh

---
+++ LVMCacheCreate.sh
@@ -0,0 +1,4 @@
+vgcreate storage /dev/sda1 /dev/sdb1 /dev/sdc1 /dev/sdd1 /dev/sde1 /dev/sdf1 /dev/sdg1 /dev/sdh1 
+lvcreate --type cache-pool --size 1200G --name cache storage /dev/sda1 /dev/sdb1 /dev/sdc1 /dev/sdd1
+lvcreate -n storage -l 100%FREE storage /dev/sde1 /dev/sdf1 /dev/sdg1 /dev/sdh1
+lvconvert --type cache --cachepool cache storage/storage
